dc2be2f6ccf6170a9b547357c0d4243df712e67e
Matt Peck <61294183+mattpeck-bit@users.noreply.github.com>
2026-09-21 10:43:29 -0400

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+
+        for(let i = 0; i < nums.length; i++){
+            for(let k = i + 1; k < nums.length; k++){
+                if (nums[i] + nums[k] == target){
+                    return [i, k];
+                }
+            }
+        }
+
+        return [];
+    }
+}
